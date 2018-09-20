1c1e5ab3c9e2387c4c71d966bf1998711395913e
Yeisonryhn <yeisonf15@gmail.com>
2018-09-20 13:31:09 -0400

    Separando cada objeto de la ventana en escenas distintas

--- Jugador/MarioScript.gd
+++ Jugador/MarioScript.gd
@@ -2,7 +2,7 @@ extends KinematicBody2D
 
 var GRAVEDAD = 200
 var velocidad = Vector2()
-
+var velCaminar = 50
 
 	
 func _ready():	
@@ -10,6 +10,19 @@ func _ready():
 	velocidad.y = 0 
 
 func _physics_process(delta):	
+
+	if (Input.is_action_pressed("moverDerecha")):
+
+		velocidad.x += velCaminar
+
+	elif(Input.is_action_pressed("moverIzquierda")):
+
+		velocidad.x += velCaminar
+
+	else:
+
+		velocidad.x=0
+	
 	velocidad.y += GRAVEDAD * delta
 	var movimiento = velocidad * delta
 	move_and_collide(movimiento)
